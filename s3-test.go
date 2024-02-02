c20764d5acbc4a4e6d5135058b7123c3ad5c7fa3
VARUN KUMAR MANIK <varunmanik1@gmail.com>
2024-02-02 13:03:46 +0800

    Create s3-test.go

---
+++ s3-test.go
@@ -0,0 +1,24 @@
+package test
+
+import (
+    "testing"
+    "github.com/gruntwork-io/terratest/modules/terraform"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestS3BucketModule(t *testing.T) {
+    terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+        // Set the path to the Terraform code that will be tested.
+        TerraformDir: "../examples/aws-s3-bucket",
+    })
+
+    // Clean up resources with "terraform destroy" at the end of the test.
+    defer terraform.Destroy(t, terraformOptions)
+
+    // Initialize and apply the Terraform code.
+    terraform.InitAndApply(t, terraformOptions)
+
+    // Run your assertions here. For example, you could check if the S3 bucket has versioning enabled.
+    bucketVersioning := terraform.Output(t, terraformOptions, "bucket_versioning")
+    assert.Equal(t, "Enabled", bucketVersioning)
+}
